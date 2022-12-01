338c2c05ae534958fa8f67203483c3e3ad3a79ff
Nick Tom Thomas <n.thomas@Nicks-MacBook-Pro-2.local>
2022-12-01 11:00:55 -0500

    finished first part of day one

--- src/challenges/day1.rs
+++ src/challenges/day1.rs
@@ -3,17 +3,43 @@ use std::{
     io::{BufRead, BufReader, Result},
 };
 
-fn challenge_one() -> Result<()> {
-    let file = File::open("assets/day1/sample.txt")?;
+fn challenge_one() -> Result<(u32)> {
+    let file = File::open("assets/day1/input.txt")?;
     let reader = BufReader::new(file);
 
-    for line in reader.lines() {
-        println!("{}", line?);
-    }
+    let values = reader
+        .lines()
+        .map(|item| item.ok())
+        .map(|item| {
+            if let Some(item) = item {
+                if item.is_empty() {
+                    ".".to_string()
+                } else {
+                    let item = format!("{item}\n");
+                    item
+                }
+            } else {
+                "something went wrong".to_string()
+            }
+        })
+        .collect::<String>();
+    let mut values = values
+        .split_terminator('.')
+        .map(|item| {
+            item.split_whitespace()
+                .map(|item| item.parse::<u32>().ok())
+                .filter(|item| item.is_some())
+                .map(|item| item.unwrap())
+                .sum::<u32>()
+        })
+        .collect::<Vec<u32>>();
 
-    Ok(())
+    values.sort();
+
+    Ok(*values.last().unwrap())
 }
 
 pub fn run() {
-    challenge_one().unwrap();
+    let output = challenge_one().unwrap();
+    println!("The output of first challenge is {output}");
 }
