cf280a784adf87b2c4914e922dff3f8e6a1c85b8
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2022-12-03 11:42:23 -0500

    started day 3 challenge

---
+++ src/challenges/day3.rs
@@ -0,0 +1,6 @@
+pub fn run() {}
+
+#[test]
+fn challenge_day_3_one() {
+    assert_eq!(2, 2);
+}
--- src/challenges/mod.rs
+++ src/challenges/mod.rs
@@ -1,5 +1,6 @@
 //mod day1;
-mod day2;
+//mod day2;
+mod day3;
 pub fn run() {
-    day2::run();
+    day3::run();
 }
