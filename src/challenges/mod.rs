0f2f8d74658ae9217621d8fb1572a74b8c7c53cd
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2022-12-10 10:16:47 -0500

    flight updates

--- src/challenges/mod.rs
+++ src/challenges/mod.rs
@@ -9,9 +9,10 @@ use std::{
 //mod day3;
 //mod day4;
 //mod day5;
-mod day6;
+//mod day6;
+mod day7;
 pub fn run() {
-    day6::run().unwrap();
+    day7::run().unwrap();
 }
 
 pub type InputType = Lines<BufReader<File>>;
