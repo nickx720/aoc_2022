69f75a9aae2a692e963633c86f0638418b54ba25
Nick <34407800+nickx720@users.noreply.github.com>
2022-12-11 22:20:40 +0400

    updates

--- src/challenges/day7.rs
+++ src/challenges/day7.rs
@@ -3,3 +3,16 @@ use super::GlobalError;
 pub fn run() -> Result<(), GlobalError> {
     Ok(())
 }
+
+#[test]
+fn challenge_day_7_test_one() -> Result<(), GlobalError> {
+    
+    assert_eq!(5, 5);
+    Ok(())
+}
+#[test]
+fn challenge_day_7_test_two() -> Result<(), GlobalError> {
+   
+    assert_eq!(23, 23);
+    Ok(())
+}
