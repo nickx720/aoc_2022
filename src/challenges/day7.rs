8cbdb65b41377be7cff80bf6668b2195ea0fa4c1
Nick Tom Thomas <n.thomas@Nicks-MacBook-Pro-2.local>
2022-12-12 08:11:55 -0500

    wip

--- src/challenges/day7.rs
+++ src/challenges/day7.rs
@@ -4,15 +4,20 @@ pub fn run() -> Result<(), GlobalError> {
     Ok(())
 }
 
-#[test]
+pub fn challenge_one() -> Result<(), GlobalError> {
+    Ok(())
+}
 
+pub fn challenge_two() -> Result<(), GlobalError> {
+    Ok(())
+}
+#[test]
 fn challenge_day_7_test_one() -> Result<(), GlobalError> {
     assert_eq!(5, 5);
     Ok(())
 }
 #[test]
 fn challenge_day_7_test_two() -> Result<(), GlobalError> {
-   
     assert_eq!(23, 23);
     Ok(())
 }
