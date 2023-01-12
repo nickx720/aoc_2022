051c5c15610c3096dd79ddbf298a7bcbc4a733df
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2023-01-12 05:22:09 -0500

    parsing logic

--- src/challenges/day14.rs
+++ src/challenges/day14.rs
@@ -4,6 +4,24 @@ pub fn run() -> Result<(), GlobalError> {
     Ok(())
 }
 
+struct Coord {
+    x: i32,
+    y: i32,
+}
+
+fn parse(input: &str) -> Vec<Vec<Coord>> {
+    let input = std::fs::read_to_string(input).unwrap();
+    input.lines().map(|line| {
+        line.split(" -> ").map(|coords| {
+            let (x,y) == coords.split_once(",").unwrap();
+            let x = x.parse().unwrap();
+            let y = y.parse().unwrap();
+            Coord {x,y}
+        }).collect()
+    }).collect()
+
+}
+
 #[test]
 fn challenge_one_day_13() -> Result<(), GlobalError> {
     assert_eq!(2, 2);
