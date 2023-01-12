318819a2613f37fa9460d90b2b7b9b6dfcd94826
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2023-01-12 05:18:14 -0500

    Day 14 boilerplate

---
+++ src/challenges/day14.rs
@@ -0,0 +1,16 @@
+use super::GlobalError;
+
+pub fn run() -> Result<(), GlobalError> {
+    Ok(())
+}
+
+#[test]
+fn challenge_one_day_13() -> Result<(), GlobalError> {
+    assert_eq!(2, 2);
+    Ok(())
+}
+#[test]
+fn challenge_two_day_13() -> Result<(), GlobalError> {
+    assert_eq!(2, 2);
+    Ok(())
+}
