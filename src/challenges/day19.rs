716c4f53c8e0793d5e5e54fa15d0ebc485fe2b2b
Nick Tom Thomas <n.thomas@Nicks-MacBook-Pro-2.local>
2023-02-01 11:28:20 -0500

    finished day 19

--- src/challenges/day19.rs
+++ src/challenges/day19.rs
@@ -1,109 +1,327 @@
-use std::collections::VecDeque;
-
 use super::GlobalError;
+use std::{
+    collections::{HashMap, VecDeque},
+    num::ParseIntError,
+    str::FromStr,
+};
 
-struct State {
-    inventory: [u16; 4],
-    bots: [u16; 4],
-    elapsed: u16,
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Material {
+    Ore,
+    Clay,
+    Obsidian,
+    Geode,
 }
 
-fn max_geodes(blueprint: &[[u16; 4]; 4]) -> u16 {
-    let max_time = 24;
-    let mut max_geodes = 0;
+#[derive(Debug, Clone, Copy)]
+struct Inventory {
+    ore_robots: usize,
+    clay_robots: usize,
+    obsidian_robots: usize,
+    geode_robots: usize,
+    ores: usize,
+    clay: usize,
+    obsidian: usize,
+    geodes: usize,
+}
 
-    let mut q = VecDeque::new();
-    q.push_back(State {
-        inventory: [0, 0, 0, 0],
-        bots: [1, 0, 0, 0],
-        elapsed: 0,
-    });
-
-    while let Some(State {
-        inventory,
-        bots,
-        elapsed,
-    }) = q.pop_front()
-    {
-        for i in 0..blueprint.len() {
-            let costs = &blueprint[i];
-            let wait_time = (0..3)
-                .map(|idx| match costs[idx] {
-                    cost if cost <= inventory[idx] => 0,
-                    _ if bots[idx] == 0 => max_time + 1,
-                    _ => (costs[idx] - inventory[idx] + bots[idx] - 1) / bots[idx],
-                })
-                .max()
-                .unwrap();
-            let new_elapsed = elapsed + wait_time + 1;
-            if new_elapsed >= max_time {
-                continue;
-            }
-
-            let mut new_inventory = [0; 4];
-            for idx in 0..bots.len() {
-                new_inventory[idx] = inventory[idx] + bots[idx] * (wait_time + 1) - costs[idx];
-            }
-            let mut new_bots = bots;
-            new_bots[i] += 1;
-
-            q.push_back(State {
-                inventory: new_inventory,
-                bots: new_bots,
-                elapsed: new_elapsed,
-            })
+impl Inventory {
+    fn new() -> Inventory {
+        Inventory {
+            ore_robots: 1,
+            clay_robots: 0,
+            obsidian_robots: 0,
+            geode_robots: 0,
+            ores: 0,
+            clay: 0,
+            obsidian: 0,
+            geodes: 0,
         }
-        let geodes = inventory[3] + bots[3] * (max_time - elapsed);
-        max_geodes = geodes.max(max_geodes);
     }
-    max_geodes
+
+    fn n_robots(&self, robot: Material) -> usize {
+        match robot {
+            Material::Ore => self.ore_robots,
+            Material::Clay => self.clay_robots,
+            Material::Obsidian => self.obsidian_robots,
+            Material::Geode => self.geode_robots,
+        }
+    }
+
+    fn unmine(self) -> Inventory {
+        let mut other = self;
+        other.ores -= other.ore_robots;
+        other.clay -= other.clay_robots;
+        other.obsidian -= other.obsidian_robots;
+        other.geodes -= other.geode_robots;
+        other
+    }
+    fn _mine(&mut self) {
+        self.ores += self.ore_robots;
+        self.clay += self.clay_robots;
+        self.obsidian += self.obsidian_robots;
+        self.geodes += self.geode_robots;
+    }
+
+    fn mine(self) -> Inventory {
+        let mut other = self;
+        other._mine();
+        other
+    }
+
+    fn build(self, blueprint: &Blueprint, robot: Material) -> Inventory {
+        let mut other = self;
+
+        let RobotCost {
+            ores,
+            clay,
+            obsidian,
+        } = blueprint.cost(robot);
+
+        other.ores -= ores;
+        other.clay -= clay;
+        other.obsidian -= obsidian;
+
+        other._mine();
+
+        match robot {
+            Material::Ore => other.ore_robots += 1,
+            Material::Clay => other.clay_robots += 1,
+            Material::Obsidian => other.obsidian_robots += 1,
+            Material::Geode => other.geode_robots += 1,
+        };
+
+        other
+    }
 }
 
-fn parse(input: &str) -> Vec<[[u16; 4]; 4]> {
-    let input = std::fs::read_to_string(input).unwrap();
-    let mut blueprints = Vec::new();
+#[derive(Debug, Clone, Copy)]
+struct RobotCost {
+    ores: usize,
+    clay: usize,
+    obsidian: usize,
+}
 
-    for line in input.lines() {
-        let mut iter = line.split_ascii_whitespace();
+#[derive(Debug)]
+struct Blueprint {
+    ore_robot: RobotCost,
+    clay_robot: RobotCost,
+    obsidian_robot: RobotCost,
+    geode_robot: RobotCost,
+}
 
-        let ore_bot_costs = [iter.nth(6).unwrap().parse().unwrap(), 0, 0, 0];
-        let clay_bot_costs = [iter.nth(5).unwrap().parse().unwrap(), 0, 0, 0];
+impl FromStr for Blueprint {
+    type Err = ParseIntError;
 
-        let obsidian_bot_costs = [
-            iter.nth(5).unwrap().parse().unwrap(),
-            iter.nth(2).unwrap().parse().unwrap(),
-            0,
-            0,
-        ];
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (_, rest) = s.split_once(':').unwrap();
+        let mut segments = rest.trim().split_terminator('.');
 
-        let geode_bot_costs = [
-            iter.nth(5).unwrap().parse().unwrap(),
-            0,
-            iter.nth(2).unwrap().parse().unwrap(),
-            0,
-        ];
-        let blueprint = [
-            ore_bot_costs,
-            clay_bot_costs,
-            obsidian_bot_costs,
-            geode_bot_costs,
-        ];
-        blueprints.push(blueprint);
+        let ore_robot_cost: usize = segments
+            .next()
+            .unwrap()
+            .trim()
+            .trim_start_matches("Each ore robot costs ")
+            .trim_end_matches(" ore")
+            .parse()?;
+
+        let clay_robot_cost: usize = segments
+            .next()
+            .unwrap()
+            .trim()
+            .trim_start_matches("Each clay robot costs ")
+            .trim_end_matches(" ore")
+            .parse()?;
+
+        let (obsidian_robot_ore_cost, obsidian_robot_clay_cost): (usize, usize) = {
+            let (ore, clay) = segments
+                .next()
+                .unwrap()
+                .trim()
+                .trim_start_matches("Each obsidian robot costs ")
+                .trim_end_matches(" clay")
+                .split_once(" ore and ")
+                .unwrap();
+            (ore.parse()?, clay.parse()?)
+        };
+
+        let (geode_robot_ore_cost, geode_robot_obsidian_cost): (usize, usize) = {
+            let (ore, clay) = segments
+                .next()
+                .unwrap()
+                .trim()
+                .trim_start_matches("Each geode robot costs ")
+                .trim_end_matches(" obsidian")
+                .split_once(" ore and ")
+                .unwrap();
+            (ore.parse()?, clay.parse()?)
+        };
+
+        Ok(Blueprint {
+            ore_robot: RobotCost {
+                ores: ore_robot_cost,
+                clay: 0,
+                obsidian: 0,
+            },
+            clay_robot: RobotCost {
+                ores: clay_robot_cost,
+                clay: 0,
+                obsidian: 0,
+            },
+            obsidian_robot: RobotCost {
+                ores: obsidian_robot_ore_cost,
+                clay: obsidian_robot_clay_cost,
+                obsidian: 0,
+            },
+            geode_robot: RobotCost {
+                ores: geode_robot_ore_cost,
+                clay: 0,
+                obsidian: geode_robot_obsidian_cost,
+            },
+        })
     }
+}
+
+impl Blueprint {
+    fn cost(&self, robot: Material) -> RobotCost {
+        match robot {
+            Material::Ore => self.ore_robot,
+            Material::Clay => self.clay_robot,
+            Material::Obsidian => self.obsidian_robot,
+            Material::Geode => self.geode_robot,
+        }
+    }
+
+    fn can_build(&self, inv: &Inventory, robot: Material) -> bool {
+        let RobotCost {
+            ores,
+            clay,
+            obsidian,
+        } = self.cost(robot);
+
+        inv.ores >= ores && inv.clay >= clay && inv.obsidian >= obsidian
+    }
+
+    /// More than just "can we build", but should we?
+    /// Returns false if we already produce 1 robots worth of this material per minute
+    /// or if we skipped building this robot before this turn
+    fn should_build(&self, inv: &Inventory, robot: Material, built: bool) -> bool {
+        if robot == Material::Geode {
+            return true;
+        };
+
+        let material_cost = |c: RobotCost| match robot {
+            Material::Ore => c.ores,
+            Material::Clay => c.clay,
+            Material::Obsidian => c.obsidian,
+            Material::Geode => unreachable!(),
+        };
+
+        let max_cost = [
+            self.ore_robot,
+            self.clay_robot,
+            self.obsidian_robot,
+            self.geode_robot,
+        ]
+        .into_iter()
+        .map(material_cost)
+        .max()
+        .unwrap_or(0);
+
+        let still_needed = inv.n_robots(robot) < max_cost;
+
+        if !built {
+            let prev_inventory = inv.unmine();
+            let skipped = self.can_build(&prev_inventory, robot);
+            still_needed && !skipped
+        } else {
+            still_needed
+        }
+    }
+}
+
+fn search(factory: &Blueprint, minutes: i32) -> usize {
+    // BFS queue
+    let mut queue = VecDeque::new();
+    queue.push_back((Inventory::new(), 0, false));
+
+    // store best geode score at each time
+    let mut cache: HashMap<i32, usize> = HashMap::new();
+    for i in 0..=minutes {
+        cache.insert(i, 0);
+    }
+
+    while let Some((inv, min, built)) = queue.pop_front() {
+        let &prior_best = cache.get(&min).unwrap();
+
+        // dead end - we already have a faster route
+        if inv.geodes < prior_best {
+            continue;
+        }
+        cache.insert(min, prior_best.max(inv.geodes));
+
+        // we're done
+        if min == minutes {
+            continue;
+        }
+
+        // greedy - build a geode whenever you can, don't expore other options
+        if factory.can_build(&inv, Material::Geode) {
+            queue.push_back((inv.build(factory, Material::Geode), min + 1, true));
+            continue;
+        }
+
+        queue.push_back((inv.mine(), min + 1, false));
+
+        for robot in [Material::Obsidian, Material::Clay, Material::Ore] {
+            // assuming we can only build 1 robot at a time
+            if factory.can_build(&inv, robot) && factory.should_build(&inv, robot, built) {
+                queue.push_back((inv.build(factory, robot), min + 1, true));
+            }
+        }
+    }
+
+    *cache.get(&minutes).unwrap()
+}
+
+fn solve_part_1(blueprints: &[Blueprint]) -> usize {
     blueprints
-}
-
-fn solution_one(input: &str) -> usize {
-    let blueprint = parse(input);
-    blueprint
         .iter()
-        .map(|blueprint| max_geodes(blueprint))
         .enumerate()
-        .map(|(idx, geodes)| (idx + 1) * usize::from(geodes))
+        .map(|(i, blueprint)| {
+            let geode_score = search(blueprint, 24);
+            geode_score * (i + 1)
+        })
         .sum()
 }
 
+fn solve_part_2(blueprints: &[Blueprint]) -> usize {
+    blueprints
+        .iter()
+        .take(3)
+        .map(|blueprint| search(blueprint, 32))
+        .product()
+}
+
+fn solve_day_19(input: &str) -> (usize, usize) {
+    let blueprints: Vec<Blueprint> = input.lines().map(|s| s.parse().unwrap()).collect();
+
+    (solve_part_1(&blueprints), solve_part_2(&blueprints))
+}
+fn solution_one(input: &str) -> usize {
+    let input = std::fs::read_to_string(input).unwrap();
+
+    solve_day_19(&input).0
+}
+fn solution_two(input: &str) -> usize {
+    let input = std::fs::read_to_string(input).unwrap();
+
+    solve_day_19(&input).1
+}
+
 pub fn run() -> Result<(), GlobalError> {
+    let input = "assets/day19/input.txt";
+    println!("{}", solution_two(input));
     Ok(())
 }
 
@@ -112,5 +330,11 @@ fn challenge_one_day_19() -> Result<(), GlobalError> {
     let input = "assets/day19/sample.txt";
     let output = solution_one(input);
     assert_eq!(33, output);
+    Ok(())
+}
+fn challenge_two_day_19() -> Result<(), GlobalError> {
+    let input = "assets/day19/sample.txt";
+    let output = solution_two(input);
+    assert_eq!(62, output);
     Ok(())
 }
