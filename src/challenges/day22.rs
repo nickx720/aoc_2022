bea15fb38f939b849a645724e48f7c2bb9b2c3fb
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2023-02-12 03:35:11 -0500

    day 22 completed

--- src/challenges/day22.rs
+++ src/challenges/day22.rs
@@ -66,7 +66,7 @@ impl Direction {
     }
 }
 
-fn wrap(map: &[Vec<Tile>], pos: &Coord, dir: &Direction) -> Coord {
+fn wrapp(map: &[Vec<Tile>], pos: &Coord, dir: &Direction) -> Coord {
     let Coord { row: dr, col: dc } = dir.offset();
     let mut curr = pos.clone();
     while let Some(tile) = map
@@ -82,6 +82,52 @@ fn wrap(map: &[Vec<Tile>], pos: &Coord, dir: &Direction) -> Coord {
         };
     }
     curr
+}
+
+fn wrap(pos: &Coord, dir: &Direction) -> (Coord, Direction) {
+    let (cube_row, cube_col, new_dir) = match (pos.row / 50, pos.col / 50, dir) {
+        (0, 1, Direction::U) => (3, 0, Direction::R),
+        (0, 1, Direction::L) => (2, 0, Direction::R),
+        (0, 2, Direction::U) => (3, 0, Direction::U),
+        (0, 2, Direction::R) => (2, 1, Direction::L),
+        (0, 2, Direction::D) => (1, 1, Direction::L),
+        (1, 1, Direction::R) => (0, 2, Direction::U),
+        (1, 1, Direction::L) => (2, 0, Direction::D),
+        (2, 0, Direction::U) => (1, 1, Direction::R),
+        (2, 0, Direction::L) => (0, 1, Direction::R),
+        (2, 1, Direction::R) => (0, 2, Direction::L),
+        (2, 1, Direction::D) => (3, 0, Direction::L),
+        (3, 0, Direction::R) => (2, 1, Direction::U),
+        (3, 0, Direction::D) => (0, 2, Direction::D),
+        (3, 0, Direction::L) => (0, 1, Direction::D),
+        _ => unreachable!(),
+    };
+
+    let (row_idx, col_idx) = (pos.row % 50, pos.col % 50);
+    let i = match dir {
+        Direction::L => 49 - row_idx,
+        Direction::R => row_idx,
+        Direction::U => col_idx,
+        Direction::D => 49 - col_idx,
+    };
+
+    let new_row = match new_dir {
+        Direction::L => 49 - i,
+        Direction::R => i,
+        Direction::U => 49,
+        Direction::D => 0,
+    };
+    let new_col = match new_dir {
+        Direction::L => 49,
+        Direction::R => 0,
+        Direction::U => i,
+        Direction::D => 49 - i,
+    };
+    let new_pos = Coord {
+        row: cube_row * 50 + new_row,
+        col: cube_col * 50 + new_col,
+    };
+    (new_pos, new_dir)
 }
 
 fn parse(input: &str) -> (Vec<Vec<Tile>>, Vec<Instruction>) {
@@ -157,7 +203,7 @@ fn solution_one(path: &str) -> i32 {
                             };
                         }
                         Tile::None => {
-                            let new_pos = wrap(&map, &pos, &dir);
+                            let new_pos = wrapp(&map, &pos, &dir);
                             if map[new_pos.row as usize][new_pos.col as usize] == Tile::Solid {
                                 break;
                             }
@@ -171,10 +217,55 @@ fn solution_one(path: &str) -> i32 {
     1000 * (pos.row + 1) + 4 * (pos.col + 1) + dir.score() as i32
 }
 
+fn solution_two(path: &str) -> i32 {
+    let input = std::fs::read_to_string(path).unwrap();
+    let (map, instructions) = parse(&input);
+
+    let start_col = map[0].iter().position(|tile| *tile == Tile::Open).unwrap() as i32;
+    let mut pos = Coord {
+        row: 0,
+        col: start_col,
+    };
+    let mut dir = Direction::R;
+    for inst in &instructions {
+        match inst {
+            Instruction::Rotate(turn) => dir = dir.turn(turn),
+            Instruction::Forward(amount) => {
+                for _ in 0..*amount {
+                    let Coord { row: dr, col: dc } = dir.offset();
+                    let new_tile = map
+                        .get((pos.row + dr) as usize)
+                        .and_then(|row| row.get((pos.col + dc) as usize))
+                        .unwrap_or(&Tile::None);
+                    match new_tile {
+                        Tile::Solid => break,
+                        Tile::Open => {
+                            pos = Coord {
+                                row: pos.row + dr,
+                                col: pos.col + dc,
+                            };
+                        }
+                        Tile::None => {
+                            let (new_pos, new_dir) = wrap(&pos, &dir);
+                            if map[new_pos.row as usize][new_pos.col as usize] == Tile::Solid {
+                                break;
+                            }
+                            pos = new_pos;
+                            dir = new_dir;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    1000 * (pos.row + 1) + 4 * (pos.col + 1) + dir.score() as i32
+}
+
 pub fn run() -> Result<(), GlobalError> {
     let path = "assets/day22/input.txt";
-    let output = solution_one(path);
-    println!("The first output is {output}");
+    let output = solution_two(path);
+    // println!("The first output is {output}");
+    println!("The second output is {output}");
     Ok(())
 }
 
