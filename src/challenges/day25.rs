f193c225ba8c2805a1bf88b3ab708d37ab850160
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2023-03-02 08:54:18 -0500

    added snafu fn

--- src/challenges/day25.rs
+++ src/challenges/day25.rs
@@ -1,5 +1,18 @@
 use super::GlobalError;
 
+fn to_snafu(decimal: i64) -> String {
+    if decimal == 0 {
+        return String::new();
+    }
+
+    let decimal_remainder = decimal % 5;
+    let snafu_digit = ['0', '1', '2', '=', '-'][decimal_remainder as usize];
+    let new_decimal = (decimal + 2) / 5;
+    let mut snafu = to_snafu(new_decimal);
+    snafu.push(snafu_digit);
+    snafu
+}
+
 fn to_decimal(snafu: &str) -> i64 {
     snafu.chars().fold(0, |decimal, snafu_digit| {
         let decimal_digit = ['=', '-', '0', '1', '2']
